506c146fa4c6161709bd6b09cbd214bddc71bf02
Ellen <supbscripter@gmail.com>
2021-01-05 19:31:09 +0000

    add bit_length variable to BitsetIterator

--- arche_tape/src/archetype_iter.rs
+++ arche_tape/src/archetype_iter.rs
@@ -7,6 +7,7 @@ mod bitset_iterator {
         phantom: PhantomData<&'a [usize]>,
         iters: Iters,
 
+        bit_length: u32,
         index: usize,
 
         bits_remaining: u32,
@@ -14,11 +15,12 @@ mod bitset_iterator {
     }
 
     impl<'a, Iters: BorrowMut<[(Iter<'a, usize>, fn(usize) -> usize)]>> BitsetIterator<'a, Iters> {
-        fn new(iters: Iters) -> Self {
+        fn new(iters: Iters, bit_length: u32) -> Self {
             Self {
                 phantom: PhantomData,
                 iters,
 
+                bit_length,
                 index: 0,
 
                 bits_remaining: 0,
@@ -61,6 +63,15 @@ mod bitset_iterator {
                 self.current_bits >>= zeros + 1;
                 self.index += zeros as usize + 1;
 
+                if self.index > self.bit_length as usize {
+                    // Hack but make sure that calling next() after None is returned continues to return None by
+                    // setting an empty iterator and making the subsequent Next calls try and call next() on it
+                    self.current_bits = 0;
+                    // Indexing by 0 should always be fine since an empty slice will never make it this far
+                    self.iters.borrow_mut()[0].0 = [].iter();
+                    return None;
+                }
+
                 return Some(self.index - 1);
             }
         }
@@ -74,7 +85,7 @@ mod bitset_iterator {
         fn empty_bitset() {
             let map: fn(_) -> _ = |x| x;
             let data = vec![];
-            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)]);
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], 0);
 
             bitset_iter.next().unwrap_none();
         }
@@ -83,7 +94,7 @@ mod bitset_iterator {
         fn single_bitset() {
             let map: fn(_) -> _ = |x| x;
             let data = vec![0b0000_1011];
-            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)]);
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], usize::BITS);
 
             assert_eq!(bitset_iter.next(), Some(0));
             assert_eq!(bitset_iter.next(), Some(1));
@@ -95,7 +106,7 @@ mod bitset_iterator {
         fn gapped_bitset() {
             let map: fn(_) -> _ = |x| x;
             let data = vec![0, 0b101];
-            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)]);
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], usize::BITS * 2);
 
             assert_eq!(bitset_iter.next(), Some(64));
             assert_eq!(bitset_iter.next(), Some(66));
@@ -109,11 +120,14 @@ mod bitset_iterator {
             let data2 = vec![0b0110_1110];
             let data3 = vec![0b1110_0110];
 
-            let mut bitset_iter = BitsetIterator::new([
-                (data1.iter(), map),
-                (data2.iter(), map),
-                (data3.iter(), map),
-            ]);
+            let mut bitset_iter = BitsetIterator::new(
+                [
+                    (data1.iter(), map),
+                    (data2.iter(), map),
+                    (data3.iter(), map),
+                ],
+                usize::BITS,
+            );
 
             assert_eq!(bitset_iter.next(), Some(1));
             assert_eq!(bitset_iter.next(), Some(5));
@@ -128,8 +142,10 @@ mod bitset_iterator {
             let data1 = vec![0b1010_1011];
             let data2 = vec![0b0111_0110];
 
-            let mut bitset_iter =
-                BitsetIterator::new([(data1.iter(), map), (data2.iter(), invert_map)]);
+            let mut bitset_iter = BitsetIterator::new(
+                [(data1.iter(), map), (data2.iter(), invert_map)],
+                usize::BITS,
+            );
 
             assert_eq!(bitset_iter.next(), Some(0));
             assert_eq!(bitset_iter.next(), Some(3));
@@ -141,11 +157,105 @@ mod bitset_iterator {
         fn all_ones() {
             let map: fn(_) -> _ = |x| x;
             let data = vec![usize::MAX];
-            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)]);
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], usize::BITS);
 
             for n in 0..usize::BITS {
                 assert_eq!(bitset_iter.next(), Some(n as _));
             }
+            bitset_iter.next().unwrap_none();
+        }
+
+        #[test]
+        fn bit_length() {
+            let map: fn(_) -> _ = |x| x;
+            let data = vec![0b101];
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], 2);
+
+            assert_eq!(bitset_iter.next(), Some(0));
+            bitset_iter.next().unwrap_none();
+        }
+
+        #[test]
+        fn long_bit_length() {
+            let map: fn(_) -> _ = |x| x;
+            let data = vec![0b0, usize::MAX];
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], usize::BITS * 2);
+
+            for n in 0..usize::BITS {
+                let n = n + 64;
+                assert_eq!(bitset_iter.next(), Some(n as _));
+            }
+            bitset_iter.next().unwrap_none();
+        }
+
+        #[test]
+        fn incorrect_bit_length() {
+            let map: fn(_) -> _ = |x| x;
+            let data = vec![0b101];
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], 2000);
+
+            assert_eq!(bitset_iter.next(), Some(0));
+            assert_eq!(bitset_iter.next(), Some(2));
+            bitset_iter.next().unwrap_none();
+        }
+
+        #[test]
+        fn returns_none_continuously_incorrect_bit_length() {
+            let map: fn(_) -> _ = |x| x;
+            let data = vec![0b101];
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], 2000);
+
+            assert_eq!(bitset_iter.next(), Some(0));
+            assert_eq!(bitset_iter.next(), Some(2));
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+        }
+
+        #[test]
+        fn returns_none_continuously_bit_length() {
+            let map: fn(_) -> _ = |x| x;
+            let data = vec![0b101];
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], 3);
+
+            assert_eq!(bitset_iter.next(), Some(0));
+            assert_eq!(bitset_iter.next(), Some(2));
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+        }
+
+        #[test]
+        fn returns_none_continuously() {
+            let map: fn(_) -> _ = |x| x;
+            let data = vec![usize::MAX];
+            // the iterator will end because of there being no more iterator left not because of the bit_length
+            let mut bitset_iter = BitsetIterator::new([(data.iter(), map)], usize::BITS);
+
+            for n in 0..usize::BITS {
+                assert_eq!(bitset_iter.next(), Some(n as _));
+            }
+
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
+            bitset_iter.next().unwrap_none();
             bitset_iter.next().unwrap_none();
         }
     }
